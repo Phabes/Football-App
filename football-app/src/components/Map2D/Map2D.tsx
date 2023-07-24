84cc0b9fa01086622528fb7b1f384bc9107cfe72
Phabes <quinlanvos1234@gmail.com>
2023-07-24 20:44:47 +0200

    retrieving data for each match and for multiple users added

--- football-app/src/components/Map2D/Map2D.tsx
+++ football-app/src/components/Map2D/Map2D.tsx
@@ -1,10 +1,49 @@
 import "./Map2D.css";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
 
-const map2D = (): JSX.Element => {
+const Map2D = (): JSX.Element => {
+  const [retrievedData, setRetrievedData] = useState<string>("");
   const params = window.location.pathname.split("/");
-  console.log(params[2]);
 
-  return <div className="map2D">MAP 2D</div>;
+  useEffect(() => {
+    const socket = io("localhost:5000");
+    socket.connect();
+
+    const handleConnect = () => {
+      console.log(`Connected ${socket.id}`);
+      socket.emit(`matches`, {
+        matchID: params[params.length - 1],
+      });
+    };
+
+    const handleMatchesData = (data: string) => {
+      setRetrievedData(data);
+      console.log(data);
+    };
+
+    const handleDisconnect = () => {
+      console.log("Disconnected");
+    };
+
+    socket.on("connect", handleConnect);
+    socket.on(`matches/${params[params.length - 1]}`, handleMatchesData);
+    socket.on("disconnect", handleDisconnect);
+
+    return () => {
+      socket.off("connect", handleConnect);
+      socket.off(`matches/${params[params.length - 1]}`, handleMatchesData);
+      socket.off("disconnect", handleDisconnect);
+      socket.disconnect();
+    };
+  }, [params]);
+
+  return (
+    <div className="map2D">
+      <div>MAP 2D</div>
+      <div>{retrievedData}</div>
+    </div>
+  );
 };
 
-export default map2D;
+export default Map2D;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const { Server } = require("socket.io");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -15,11 +16,13 @@ for (let i = 0; i < 120; i++) {
   });
 }
 const matches = [];
+let currentMatchID = 0;
 for (let i = (clubs.length - 2) / 2 + 1; i < clubs.length - 2 + 1; i += 2) {
   matches.push({
-    id: i / 2,
-    firstTeam: clubs[i],
-    secondTeam: clubs[i + 1],
+    id: currentMatchID++,
+    homeTeam: clubs[i],
+    awayTeam: clubs[i + 1],
+    subscribers: [],
   });
 }
 const itemsPerPage = 20;
@@ -42,35 +45,96 @@ app.post("/matches", (req, res) => {
   const start = pageNumber * itemsPerPage;
   const end = Math.min((pageNumber + 1) * itemsPerPage, matches.length);
   const wantedMatches = matches.slice(start, end);
-  res
-    .status(200)
-    .json({ matches: wantedMatches, hasMore: end < matches.length });
+  const finalWantedMatches = wantedMatches.map(
+    ({ ["subscribers"]: _, ...rest }) => rest
+  );
+  res.status(200).json({
+    matches: finalWantedMatches,
+    hasMore: end < matches.length,
+  });
 });
 
 app.post("/newMatch", (req, res) => {
   const { match } = req.body;
   const alreadyUsed = matches.some((singleMatch) => {
     return (
-      singleMatch.firstTeam.id == match.firstTeam.id ||
-      singleMatch.firstTeam.id == match.secondTeam.id ||
-      singleMatch.secondTeam.id == match.firstTeam.id ||
-      singleMatch.secondTeam.id == match.secondTeam.id
+      singleMatch.homeTeam.id == match.homeTeam.id ||
+      singleMatch.homeTeam.id == match.awayTeam.id ||
+      singleMatch.awayTeam?.id == match.homeTeam.id ||
+      singleMatch.awayTeam?.id == match.awayTeam.id
     );
   });
   if (!alreadyUsed) {
     const footballMatch = {
-      id: matches.length,
-      firstTeam: match.firstTeam,
-      secondTeam: match.secondTeam,
+      id: currentMatchID++,
+      homeTeam: match.homeTeam,
+      awayTeam: match.awayTeam,
     };
     matches.push(footballMatch);
-    console.log(JSON.stringify(matches, null, 5));
     res.status(200).json({ success: true });
   } else {
     res.status(200).json({ success: false });
   }
 });
 
-app.listen(PORT, () => {
+const appServer = app.listen(PORT, () => {
   console.log(`SERVER STARTED ON PORT ${PORT}`);
 });
+
+const io = new Server(appServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const refreshMatchesData = () => {
+  matches.forEach((match, i) => {
+    const randomNumber = Math.random();
+    match.subscribers.forEach((socket) => {
+      socket.emit(`matches/${i}`, `matches/${i} ${randomNumber}`);
+    });
+  });
+};
+
+const intervalId = setInterval(() => {
+  try {
+    refreshMatchesData();
+  } catch (error) {
+    console.error("Error occurred during refreshing matches data:", error);
+  }
+}, 2000);
+
+const findMatchByID = (matchID) => {
+  return matches.find((match) => match.id == matchID);
+};
+
+const deleteIfExists = (arr, value) => {
+  const index = arr.indexOf(value);
+  if (index !== -1) arr.splice(index, 1);
+};
+
+io.on("connection", (socket) => {
+  console.log(`user id ${socket.id}`);
+
+  socket.on("disconnect", () => {
+    matches.forEach((match) => {
+      deleteIfExists(match.subscribers, socket);
+    });
+    console.log("A user disconnected");
+  });
+
+  socket.on(`matches`, (data) => {
+    console.log("Received message:", data);
+    const match = findMatchByID(data.matchID);
+    if (!match.subscribers.includes(socket)) match.subscribers.push(socket);
+  });
+});
+
+// const disconnectAllClients = () => {
+//   const connectedSockets = Object.values(io.sockets.sockets);
+
+//   connectedSockets.forEach((socket) => {
+//     socket.disconnect(true); // Pass true to close the underlying connection
+//   });
+// };
+// disconnectAllClients();
